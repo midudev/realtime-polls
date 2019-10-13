f9b0b35d1476ae6c0ca248711ff83a587c8aa02c
midudev <miduga@gmail.com>
2019-10-13 11:37:41 +0200

    Create API for voting and retrieving votes

---
+++ pages/api/poll.js
@@ -0,0 +1,48 @@
+const nanoid = require('nanoid')
+
+const voters = []
+const votes = { react: 0, vue: 0, angular: 0, other: 0 }
+const POLLS = ['holyjs']
+
+export default (req, res) => {
+  if (req.method === 'GET') {
+    const totalVotes = Object.values(votes).reduce((prev, num) => prev + num)
+    const percentatge = Object.keys(votes).reduce((percentatges, answer) => {
+      const calculation = totalVotes === 0 ? totalVotes : votes[answer] / totalVotes * 100
+      percentatges[answer] = Math.round(calculation * 100) / 100
+      return percentatges
+    }, {})
+
+    res.json({
+      raw: votes,
+      percentatge,
+      totalVotes
+    })
+  }
+
+  if (req.method === 'POST') {
+    const userId = nanoid()
+    const { _vt: userIdFromCookie } = req.cookies
+    const { poll, answer } = req.body
+
+    const isCorrectPoll = POLLS.includes(poll)
+    if (!isCorrectPoll) {
+      return res.status(400).json({ STATUS: 'Incorrect poll' })
+    }
+
+    const hasUserAlreadyVoted = voters.includes(userIdFromCookie)
+    if (hasUserAlreadyVoted) {
+      return res.status(400).json({ STATUS: 'Already voted' })
+    }
+
+    const answerVoted = typeof votes[answer] !== 'undefined' ? votes[answer]++ : false
+
+    if (answerVoted === false) {
+      return res.status(400).json({ STATUS: 'Invalid answer' })
+    }
+
+    voters.push(userId) // save voter
+    res.setHeader('Set-Cookie', `_vt=${userId}`) // store the cookie
+    res.json({ STATUS: 'OK' }) // send the response
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import Link from 'next/link'
+
+export default function () {
+  return <Link href='holyjs'><a>Holy JS</a></Link>
+}
